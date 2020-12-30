61f1a265b0fbef376ca7cb9e458badb57335d9ee
foxhound-cmd <dada2-63@hotmail.fr>
2020-12-30 21:27:17 +0100

    update project object and create repo init h2

---
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.linkodev.ppmtool.repositories;
+
+import io.linkodev.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+}
