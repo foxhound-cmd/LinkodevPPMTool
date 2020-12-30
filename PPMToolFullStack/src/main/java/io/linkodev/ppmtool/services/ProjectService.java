703e62ac3e9cc572683a82b314d74902a2f7b914
foxhound-cmd <dada2-63@hotmail.fr>
2020-12-30 22:13:54 +0100

    init project controller and services

---
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package io.linkodev.ppmtool.services;
+
+import io.linkodev.ppmtool.domain.Project;
+import io.linkodev.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
