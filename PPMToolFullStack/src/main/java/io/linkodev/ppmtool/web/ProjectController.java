429e2dc0470e21b212bafabbb3e4182e640b4677
foxhound-cmd <dada2-63@hotmail.fr>
2020-12-31 22:50:49 +0100

    error format json for project object

--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/web/ProjectController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -24,10 +28,15 @@ public class ProjectController {
     public ResponseEntity<?> createdNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid project object",HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            result.getFieldErrors().forEach(error -> errorMap.put(error.getField(), error.getDefaultMessage()));
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+        return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
 }
