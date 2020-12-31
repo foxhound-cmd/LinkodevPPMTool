c3f06a42de911c84efa51bdb2caf358450a050c9
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-01 00:44:31 +0100

    exception handler unique constraint project

--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectService.java
@@ -1,9 +1,12 @@
 package io.linkodev.ppmtool.services;
 
 import io.linkodev.ppmtool.domain.Project;
+import io.linkodev.ppmtool.exceptions.ProjectIdException;
 import io.linkodev.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class ProjectService {
@@ -12,6 +15,15 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+
+        try {
+
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()
+            + "' already exist");
+        }
     }
 }
