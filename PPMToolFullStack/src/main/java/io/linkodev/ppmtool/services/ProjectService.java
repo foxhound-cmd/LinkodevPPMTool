9a267f460452200599d77789f49d8871cd831d09
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-01 01:43:56 +0100

    remove project from database with new api request

--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectService.java
@@ -39,4 +39,15 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete project with ID '" + projectId.toUpperCase() +
+                    "'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/web/ProjectController.java
@@ -50,4 +50,10 @@ public class ProjectController {
     public Iterable<Project> getAllProjects() {
         return projectService.findAllProjects();
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<>("Project with ID: '" + projectId.toUpperCase() + "' was deleted", HttpStatus.OK);
+    }
 }
