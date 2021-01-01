3ed9efd6d29a441303e6f33761526ca42ecfb7bd
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-01 21:43:59 +0100

    bootstrap 4 add to the project

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
