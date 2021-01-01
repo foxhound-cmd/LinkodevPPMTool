457eee59f97a5a82eb64bf158162b52c2600af79
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-01 21:36:29 +0100

    create navbar projectItem component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Dashboard></Dashboard>
     </div>
   );
