8dffa076e99e86bd3981ab700b20a49d12f93f70
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-03 19:49:13 +0100

    add route for react

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Dashboard></Dashboard>
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <Route exact path="/dashboard" component={Dashboard}></Route>
+        <Route exact path="/addProject" component={AddProject}></Route>
+      </div>
+    </Router>
   );
 }
 
