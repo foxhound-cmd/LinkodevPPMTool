ab8f631333e851fa63f33b0285d13d3894a0cae9
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-13 21:14:25 +0100

    new MySQL setup instead of H2, add project not found handler

--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectTaskService.java
@@ -4,7 +4,9 @@ package io.linkodev.ppmtool.services;
 import io.linkodev.ppmtool.domain.Backlog;
 import io.linkodev.ppmtool.domain.Project;
 import io.linkodev.ppmtool.domain.ProjectTask;
+import io.linkodev.ppmtool.exceptions.ProjectNotFoundException;
 import io.linkodev.ppmtool.repositories.BacklogRepository;
+import io.linkodev.ppmtool.repositories.ProjectRepository;
 import io.linkodev.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,27 +22,41 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        try {
 
-        projectTask.setBacklog(backlog);
-        Integer backlogSequence = backlog.getPTSequence();
-        projectTask.setProjectSequence(projectIdentifier + "-" + (++backlogSequence));
-        projectTask.setProjectIdentifier(projectIdentifier);
-        backlog.setPTSequence(backlogSequence);
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 
-        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
-            projectTask.setPriority(3);
+            projectTask.setBacklog(backlog);
+            Integer backlogSequence = backlog.getPTSequence();
+            projectTask.setProjectSequence(projectIdentifier + "-" + (++backlogSequence));
+            projectTask.setProjectIdentifier(projectIdentifier);
+            backlog.setPTSequence(backlogSequence);
+
+            if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+                projectTask.setPriority(3);
+            }
+
+            if (projectTask.getStatus() == null || projectTask.getStatus().equals("")) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not Found with identifier " + projectIdentifier.toUpperCase());
         }
-
-        if (projectTask.getStatus() == null || projectTask.getStatus().equals("")) {
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
      public Iterable<ProjectTask> findBacklogById(String backlog_id) {
+
+        Project project = projectRepository.findByProjectIdentifier(backlog_id);
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID : '" + backlog_id.toUpperCase() + "'does not exist");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
     }
 }
