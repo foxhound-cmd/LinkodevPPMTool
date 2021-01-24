94adb408be1309dc4ccd9e3c13e74f6ec1ce74e6
foxhound-cmd <dada2-63@hotmail.fr>
2021-01-24 21:59:57 +0100

    fix delete projectTask

--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/domain/ProjectTask.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/domain/ProjectTask.java
@@ -34,7 +34,7 @@ public class ProjectTask {
 
     private Date update_at;
 
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
     @JsonIgnore
     private Backlog backlog;
--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/services/ProjectTaskService.java
@@ -96,12 +96,6 @@ public class ProjectTaskService {
 
     public void deletePTByProjectSequence(String backlog_id, String pt_id) {
         ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
-
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> pts = backlog.getProjectTaskList();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
-
         projectTaskRepository.delete(projectTask);
     }
 }
