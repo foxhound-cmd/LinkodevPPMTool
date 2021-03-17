0e3361634ed6c678cc1ad54a79caafcdc0c59fc7
foxhound-cmd <dada2-63@hotmail.fr>
2021-03-17 22:00:35 +0100

    generate jwt token

--- PPMToolFullStack/src/main/java/io/linkodev/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/io/linkodev/ppmtool/web/UserController.java
@@ -1,6 +1,9 @@
 package io.linkodev.ppmtool.web;
 
 import io.linkodev.ppmtool.domain.User;
+import io.linkodev.ppmtool.payload.JWTLoginSucessResponse;
+import io.linkodev.ppmtool.payload.LoginRequest;
+import io.linkodev.ppmtool.security.JwtTokenProvider;
 import io.linkodev.ppmtool.services.MapValidationErrorService;
 import io.linkodev.ppmtool.services.UserService;
 import io.linkodev.ppmtool.validator.UserValidator;
@@ -8,6 +11,10 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static io.linkodev.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -29,6 +38,32 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSucessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
