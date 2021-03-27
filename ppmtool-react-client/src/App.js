624463a05cd6c2d74963fe70aef9c75565a4b71b
foxhound-cmd <dada2-63@hotmail.fr>
2021-03-28 00:56:19 +0100

    handle token after login

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -13,42 +13,64 @@ import UpdateProjectTask from "./components/projectBoard/projectTask/UpdateProje
 import Landing from "./components/layout/Landing";
 import Register from "./components/userManagement/Register";
 import Login from "./components/userManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWToken from "./securityUtils/setJWToken";
+import { Component } from "react";
+import { SET_CURRENT_USER } from "./actions/types";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header></Header>
-          <Route exact path="/" component={Landing}></Route>
-          <Route exact path="/login" component={Login}></Route>
-          <Route exact path="/register" component={Register}></Route>
-          <Route exact path="/dashboard" component={Dashboard}></Route>
-          <Route exact path="/addProject" component={AddProject}></Route>
-          <Route
-            exact
-            path="/updateProject/:id"
-            component={UpdateProject}
-          ></Route>
-          <Route
-            exact
-            path="/projectBoard/:id"
-            component={ProjectBoard}
-          ></Route>
-          <Route
-            exact
-            path="/addProjectTask/:id"
-            component={AddProjectTask}
-          ></Route>
-          <Route
-            exact
-            path="/updateProjectTask/:backlog_id/:pt_id"
-            component={UpdateProjectTask}
-          ></Route>
-        </div>
-      </Router>
-    </Provider>
-  );
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken,
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    //window.location.href = "/";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header></Header>
+            <Route exact path="/" component={Landing}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/dashboard" component={Dashboard}></Route>
+            <Route exact path="/addProject" component={AddProject}></Route>
+            <Route
+              exact
+              path="/updateProject/:id"
+              component={UpdateProject}
+            ></Route>
+            <Route
+              exact
+              path="/projectBoard/:id"
+              component={ProjectBoard}
+            ></Route>
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            ></Route>
+            <Route
+              exact
+              path="/updateProjectTask/:backlog_id/:pt_id"
+              component={UpdateProjectTask}
+            ></Route>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
